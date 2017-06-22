db1c3ef82a18b3c0887ef3a687e9703acab24636
Joah Gerstenberg <me@joahg.com>
2017-06-22 16:55:26 -0500

    new paths + redirects for all pages

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.card .card-inner').on('click', function(e) {
+    $('input[name="SelectedPlan"]').attr('value', $(this).attr('data-plan'));
+    $('textarea[name="Description"]').val({
+      'Ecommerce Website Build': 'I\'m interested in an Ecommerce website design/build',
+      'Ecommerce Hourly': 'I\'m interested in hourly implementation',
+      'Ecommerce Retainer': 'I\'m interested in a monthly retainer'
+    }[$(this).attr('data-plan')]);
+    $('html,body').animate({
+      scrollTop: $('#contact').offset().top
+    }, 1000);
+  });
+});
